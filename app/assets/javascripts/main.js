591c9bf1e5052c8a1a5c82e5ef3473711b7937e1
Laura <laura.s.chavez@outlook.com>
2018-09-04 19:52:02 -0400

    new code

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
     sub_btn();
-    
-    });
-    
     function sub_btn(){
         $('#sub_form').on('ajax:success', function (e){
         window.location.reload()
     })
     
     }
+    $.ajax({
+        url: "/test"
+      }).done(function(html) {
+        return $("#results").append(html);
+      });
+    });
+    
+    
     
