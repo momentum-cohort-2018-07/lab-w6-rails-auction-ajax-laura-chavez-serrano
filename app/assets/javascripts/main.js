596cd2150384ca09a1d5f676b4764b81b0e4eca7
Laura <laura.s.chavez@outlook.com>
2018-09-04 20:08:43 -0400

    new code in js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
     function sub_btn(){
         $('#sub_form').on('ajax:success', function (e){
         window.location.reload()
-    })
+        })
     
     }
     $.ajax({
-        url: "/test"
-      }).done(function(html) {
-        return $("#results").append(html);
-      });
+        url: "/items"
+    }).done(function(html) {
+        return $("table").append(html);
     });
+});
     
     
     
